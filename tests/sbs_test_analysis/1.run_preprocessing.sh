8627d42ae9e20de9035de7f3b5586dcff1c9884c
tubhi <t.ubhi04@gmail.com>
2025-06-26 21:09:44 -0400

    sbs test analysis

--- tests/sbs_test_analysis/1.run_preprocessing.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Run only the preprocess rules
-snakemake --use-conda --cores all \
-    --snakefile "config/Snakefile" \
-    --configfile "config/config.yml" \
-    --rerun-triggers mtime \
-    --until all_preprocess
